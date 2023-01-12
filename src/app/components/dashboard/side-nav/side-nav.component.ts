be2f17ade5c7c3e3af554a2cb5ab8e16ef85cb7e
Teazz <etosin45@gmail.com>
2023-01-12 14:51:02 +0100

    Made the dashboard navigation more functional and responsive on mobile

--- src/app/components/dashboard/side-nav/side-nav.component.ts
+++ src/app/components/dashboard/side-nav/side-nav.component.ts
@@ -3,15 +3,30 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.css']
+  styleUrls: ['./side-nav.component.css'],
 })
 export class SideNavComponent implements OnInit {
 
-  constructor(private routes: Router) { }
+  isActive = false;
+  smallScreenDisplay = 'none';
+  showLogo = true
 
-  ngOnInit(): void {
+  constructor(private routes: Router) {}
+
+  ngOnInit(): void {}
+  navigateToHome() {
+    this.routes.navigateByUrl('./LandingPage');
   }
-navigateToHome(){
-  this.routes.navigateByUrl('./LandingPage')
-}
+
+  toggleOn() {
+    this.isActive = true;
+    this.smallScreenDisplay = 'block';
+    this.showLogo = false
+  }
+
+  toggleOff() {
+    this.isActive = false;
+    this.smallScreenDisplay = 'none';
+    this.showLogo = true
+  }
 }
